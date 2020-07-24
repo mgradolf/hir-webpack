62ed736e1b981d475c49db5cb162304c81f21c59
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2020-07-24 14:46:57 +0600

    removed old src2 folder

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { App } from './App'
 import * as Sentry from '@sentry/react'
 import { createStore } from '~/store'
+import * as serviceWorker from '~/serviceWorker'
 
 const { store } = createStore()
 
@@ -15,3 +16,8 @@ if (process.env.REACT_APP_SENTRY_RELEASE && process.env.REACT_APP_SENTRY_DSN) {
 
 const root = document.getElementById('root')
 render(<App store={store} />, root)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
