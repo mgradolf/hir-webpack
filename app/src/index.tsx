8393200f118cafb89afaa5b19c736e881f7abacc
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-07-30 23:39:23 +0600

    Minor amend in redux approach
    - Create store once then pass it through params

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,9 +3,11 @@ import { render } from 'react-dom'
 import * as Sentry from '@sentry/react'
 import { App } from '~/App'
 import * as serviceWorker from '~/serviceWorker'
-import { store } from '~/store'
+import { createStore } from '~/store'
 import registeGlobalhttpErrorHandlerr from '~/api-wrappers/RegisteGlobalhttpErrorHandlerr'
+import 'antd/dist/antd.css'
 
+const { store, history } = createStore()
 if (process.env.REACT_APP_SENTRY_RELEASE && process.env.REACT_APP_SENTRY_DSN) {
   Sentry.init({
     release: process.env.REACT_APP_SENTRY_RELEASE,
@@ -13,10 +15,10 @@ if (process.env.REACT_APP_SENTRY_RELEASE && process.env.REACT_APP_SENTRY_DSN) {
   })
 }
 
-registeGlobalhttpErrorHandlerr()
+registeGlobalhttpErrorHandlerr(store)
 
 const root = document.getElementById('root')
-render(<App store={store} />, root)
+render(<App store={store} history={history} />, root)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
