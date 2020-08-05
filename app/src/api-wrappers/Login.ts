cfe15be34064b5d0f85414a568d076d000e1cfeb
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-08-05 15:59:10 +0600

    Update login response type

--- app/src/api-wrappers/Login.ts
+++ app/src/api-wrappers/Login.ts
@@ -1,6 +1,6 @@
 import { login } from '@packages/api/lib/Login'
 
-type LoginResponse = { token: string } // TODO: More to define here, as we only know token for now
+type LoginResponse = { data: { data: { token: string } } } // TODO: More to define here, as we only know token for now
 type Response<T> = [T | undefined, unknown | undefined] // TODO: should be exported from somewhere more generic
 
 export function loginWrapper(
