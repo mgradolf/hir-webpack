e09658c01891e440608bc2c83acbee39ed9e8881
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2020-08-10 13:47:09 +0600

    event bus instance renamed

--- packages/api/src/utils/CallApi.ts
+++ packages/api/src/utils/CallApi.ts
@@ -1,18 +1,20 @@
 import axios, { AxiosRequestConfig } from "axios"
 import { ApiConfig, ErrorType } from "./Interfaces"
 import { handleResponse } from "./HandleResponse"
-import eventBus from "./GlobalHttpErrorEventBus"
+import apiErroreEventBus from "./GlobalHttpErrorEventBus"
 
 export default async function callApi(config: ApiConfig): Promise<[any, any]> {
   const requestConfig: AxiosRequestConfig = <AxiosRequestConfig>config
   requestConfig.withCredentials = true
+  apiErroreEventBus.publish(null)
+
   const [response, error] = await handleResponse(axios.request(requestConfig))
   // console.log("requestConfig", requestConfig)
   // console.log("response ", response)
   // console.log("error ", error)
 
   if (error && error.type === ErrorType.GLOBAL) {
-    eventBus.publish(error)
+    apiErroreEventBus.publish(error)
     return [undefined, undefined]
   }
   return [response, error]
