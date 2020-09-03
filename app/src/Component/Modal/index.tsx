82299833f0998a88dc8d9ee2ce5c887470a6fc1c
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2020-09-04 00:56:41 +0600

    finally, focused modal done

--- app/src/Component/Modal/index.tsx
+++ app/src/Component/Modal/index.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react"
 import style from "~/Component/Modal/modal.module.scss"
 import { Row, Col, Spin, Card } from "antd"
 import zIndexLevel from "~/utils/zIndex"
+import FocusTrap from "focus-trap-react"
+import { Options as FocusTrapOptions } from "focus-trap"
 
 interface IModalProp {
   closable?: boolean
@@ -23,12 +25,10 @@ export default function ({
   loadingTip = "Loading...",
   apiCallInProgress = false
 }: IModalProp) {
-  const [visibility, setvisibility] = useState(true)
-  const closeOnClickOutside = () => {
-    if (closable) {
-      setvisibility(false)
-    }
-  }
+  const focusTrapOption = {
+    allowOutsideClick: () => true,
+    fallbackFocus: () => document.getElementById("modalContainer")
+  } as FocusTrapOptions
 
   useEffect(() => {
     if (showModal) {
@@ -39,22 +39,18 @@ export default function ({
       document.documentElement.style.height = ""
       document.documentElement.style.overflow = ""
     }
-  }, [showModal, visibility])
+  }, [showModal])
   return (
     <>
-      {showModal && visibility && (
-        <div aria-modal={true} role={"dialog"}>
-          <div className={style.modal} style={{ zIndex }} onClick={closeOnClickOutside}></div>
-          <div className={style.modal_content} style={{ zIndex: zIndex + 1 }}>
-            {closable && (
-              <span className={style.modal__close} onClick={() => setvisibility(false)}>
-                &times;
-              </span>
-            )}
-            <Row>
-              <Col flex="auto"></Col>
-              {loading && <ModalLoading {...{ width, loadingTip }} />}
-              {!loading && (
+      {showModal && (
+        <FocusTrap
+          focusTrapOptions={focusTrapOption}
+          children={
+            <div id="modalContainer" tabIndex={-1}>
+              <div tabIndex={0} className={style.modal} style={{ zIndex }}></div>
+              <Row className={style.modal_content} style={{ zIndex: zIndex + 1 }}>
+                <Col flex="auto"></Col>
+                {loading && <ModalLoading {...{ width, loadingTip }} />}
                 <Col flex={width}>
                   {children}
                   {apiCallInProgress && (
@@ -65,11 +61,12 @@ export default function ({
                     </div>
                   )}
                 </Col>
-              )}
-              <Col flex="auto"></Col>
-            </Row>
-          </div>
-        </div>
+
+                <Col flex="auto"></Col>
+              </Row>
+            </div>
+          }
+        ></FocusTrap>
       )}
     </>
   )
