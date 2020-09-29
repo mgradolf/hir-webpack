36eaba1ebd8759c30ea87aca54e954e2986cb5ad
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2020-09-29 17:39:54 +0600

    store renamed

---
+++ app/src/Pages/Offering/Financial/index.tsx
@@ -0,0 +1,190 @@
+import React, { useState, useEffect } from "react"
+
+import { RouteComponentProps } from "react-router"
+import { Row, Col, Typography, Space, Dropdown } from "antd"
+import { searchOfferingFinancial } from "~/ApiServices/Service/OfferingService"
+import styles from "~/Pages/Offering/Financial/Financial.module.scss"
+import ResponsiveTable from "~/Component/Common/ResponsiveTable"
+import OfferingFinancialModalOpenButton from "~/Component/Offering/Financial/OfferingFinancialModalOpenButton"
+import { eventBus, REFRESH_OFFERING_FINANCIAL_PAGE } from "~/utils/EventBus"
+import FinancialMenu from "~/Component/Offering/Financial/FinancialMenu"
+import { DownOutlined } from "@ant-design/icons"
+
+const { Title } = Typography
+
+function OfferingFinancialPage(props: RouteComponentProps<{ id: string }>) {
+  const columns = [
+    {
+      title: "Description",
+      dataIndex: "Description"
+    },
+    {
+      title: "Category",
+      dataIndex: "FinancialCategoryType"
+    },
+    {
+      title: "Basis",
+      dataIndex: "FinancialBasisType"
+    },
+    {
+      title: "Amount",
+      dataIndex: "ItemUnitAmount"
+    },
+    {
+      title: "Type",
+      dataIndex: "FinancialType"
+    },
+    {
+      title: "Optional?",
+      dataIndex: "IsOptional",
+      render: (value: boolean) => (value ? "Yes" : "No")
+    },
+    {
+      title: "Taxable?",
+      dataIndex: "IsTaxable",
+      render: (value: boolean) => (value ? "Yes" : "No")
+    },
+    {
+      title: "Weight",
+      dataIndex: "Weight"
+    },
+    {
+      title: "Active",
+      dataIndex: "IsActive",
+      render: (value: boolean) => (value ? "Yes" : "No")
+    },
+    {
+      title: "GL Account",
+      dataIndex: "GLAccount"
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (record: any) => (
+        <Space size="middle">
+          <Dropdown
+            overlay={<FinancialMenu offeringId={record.ApplyToID} financialId={record.FinancialID} />}
+            trigger={["click"]}
+          >
+            <a href="/" className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+              Others <DownOutlined />
+            </a>
+          </Dropdown>
+        </Space>
+      )
+    }
+  ]
+
+  const expandableRowRender = (data: { [key: string]: any }, display: boolean): JSX.Element => {
+    return (
+      <>
+        {display && (
+          <div style={{ border: "1px solid", padding: "5px" }}>
+            <Row>
+              <Col span="8">Category:</Col>
+              <Col span="16">{data.FinancialCategoryType}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Basis:</Col>
+              <Col span="16">{data.FinancialBasisType}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Amount:</Col>
+              <Col span="16">{data.ItemUnitAmount}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Type:</Col>
+              <Col span="16">{data.FinancialType}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Optional:</Col>
+              <Col span="16">{data.IsOptional ? "Yes" : "No"}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Taxable:</Col>
+              <Col span="16">{data.IsTaxable ? "Yes" : "No"}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Weight:</Col>
+              <Col span="16">{data.Weight}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">Active:</Col>
+              <Col span="16">{data.IsActive ? "Yes" : "No"}</Col>
+            </Row>
+
+            <Row>
+              <Col span="8">GL Account:</Col>
+              <Col span="16">{data.GLAccount}</Col>
+            </Row>
+          </div>
+        )}
+      </>
+    )
+  }
+
+  const offeringID = props.match.params.id
+  const [loading, setLoading] = useState<boolean>(false)
+  const [offeringFinancialItems, setOfferingFinancialItems] = useState<Array<any>>([])
+
+  useEffect(() => {
+    const loadOfferingFinancials = async function () {
+      setLoading(true)
+
+      const result = await searchOfferingFinancial(Number(offeringID))
+
+      if (result && result.success) {
+        setLoading(false)
+        setOfferingFinancialItems(
+          result.data.map((x: any, index: number) => {
+            x.key = index
+            return x
+          })
+        )
+      }
+    }
+    eventBus.subscribe(REFRESH_OFFERING_FINANCIAL_PAGE, loadOfferingFinancials)
+    eventBus.publish(REFRESH_OFFERING_FINANCIAL_PAGE)
+    return () => {
+      eventBus.unsubscribe(REFRESH_OFFERING_FINANCIAL_PAGE)
+    }
+  }, [offeringID])
+
+  return (
+    <div className="site-layout-content">
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col className="gutter-row" xs={24} sm={24} md={12}>
+          <Title level={3}>Manage Offering Financial</Title>
+        </Col>
+        <Col className={`gutter-row ${styles.textAlignRight}`} xs={24} sm={24} md={12}>
+          <OfferingFinancialModalOpenButton offeringId={parseInt(offeringID)} />
+        </Col>
+      </Row>
+
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className={`${styles.paddingTop10px}  ${styles.margin0px}`}>
+        <Col className={`gutter-row ${styles.offeringFinancialDetails}`} xs={24} sm={24} md={24}>
+          <ResponsiveTable
+            columns={columns}
+            dataSource={offeringFinancialItems}
+            loading={loading}
+            expandableRowRender={expandableRowRender}
+            bordered
+            pagination={{ position: ["topLeft"], pageSize: 20 }}
+            breakpoints={["md", "lg", "xl", "xxl"]}
+            responsiveColumnIndices={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            scroll={{ y: 600 }}
+          />
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default OfferingFinancialPage
---
+++ app/src/Pages/Offering/Tag/index.tsx
@@ -0,0 +1,241 @@
+import React, { useState, useEffect } from "react"
+import { RouteComponentProps } from "react-router-dom"
+import { getTagTypes } from "~/ApiServices/Service/RefLookupService"
+import { getTags, getParentTags, addTagIntoEntity, removeTagFromEntity } from "~/ApiServices/Service/TagService"
+import { Form, Card, Select, Input, Switch, Row, Col, Button, Spin } from "antd"
+import { CloseOutlined, CheckOutlined } from "@ant-design/icons"
+import TagsTable from "~/Component/Offering/Tag/TagsTable"
+import { eventBus, REFRESH_OFFERING_TAG_PAGE } from "~/utils/EventBus"
+import { hidden } from "~/utils/style"
+import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
+
+interface IFieldNames {
+  EntityType: string
+  EntityID: string
+  TagTypeID: string
+  IsSelected: string
+  Tag: string
+}
+
+const fieldNames = {
+  EntityType: "EntityType",
+  EntityID: "EntityID",
+  TagTypeID: "TagTypeID",
+  IsSelected: "IsSelected",
+  Tag: "Tag"
+}
+
+interface IinitialValues {
+  EntityType: string
+  EntityID: number
+  IsSelected: boolean
+  Tag: string
+  TagTypeID: Array<number | string>
+}
+export default function (props: RouteComponentProps<{ id: string }>) {
+  const [tagTypes, setTagTypes] = useState<Array<any>>([])
+
+  const [offeringTags, setOfferingTags] = useState<Array<any>>([])
+  const [parentTags, setParentTags] = useState<Array<any>>([])
+
+  const [loadingTagTypes, setLoadingTagTypes] = useState(false)
+  const [loadingOfferingTagSearchResults, setLoadingOfferingTagSearchResults] = useState(false)
+  const [loadingParentTagSearchResults, setLoadingParentTagSearchResults] = useState(false)
+
+  const [formInstance] = Form.useForm()
+  const initialValues: IinitialValues = {
+    EntityType: "Offering",
+    EntityID: parseInt(props.match.params.id),
+    IsSelected: true,
+    Tag: "*",
+    TagTypeID: []
+  }
+
+  const loadTagTypes = async () => {
+    setLoadingTagTypes(true)
+    const response = await getTagTypes()
+    if (response.success) {
+      setTagTypes(response.data)
+    }
+    setLoadingTagTypes(false)
+  }
+
+  const searchTags = async () => {
+    clearResult()
+    Promise.all([searchOfferingTags(), searchParentTags()])
+  }
+
+  useEffect(() => {
+    eventBus.subscribe(REFRESH_OFFERING_TAG_PAGE, () => {
+      loadTagTypes()
+      searchTags()
+    })
+    eventBus.publish(REFRESH_OFFERING_TAG_PAGE)
+    return () => {
+      eventBus.unsubscribe(REFRESH_OFFERING_TAG_PAGE)
+    }
+    // eslint-disable-next-line
+  }, [props.match.params.id])
+
+  const resetForm = () => {
+    clearResult()
+    formInstance.resetFields()
+  }
+
+  const clearResult = () => {
+    setParentTags([])
+    setOfferingTags([])
+  }
+
+  const convertFormFieldIntoSearchParam = (formFields: { [key: string]: any }): IinitialValues => {
+    const searchParams = formFields as IinitialValues
+    searchParams.TagTypeID = searchParams.TagTypeID.map((x) => {
+      if (typeof x === "string") {
+        return parseInt(x.split(";")[0])
+      }
+      return x
+    })
+    return searchParams
+  }
+
+  const searchOfferingTags = async () => {
+    setLoadingOfferingTagSearchResults(true)
+    const response = await getTags(convertFormFieldIntoSearchParam(formInstance.getFieldsValue()))
+    setLoadingOfferingTagSearchResults(false)
+    if (response.success && Array.isArray(response.data)) {
+      setOfferingTags(
+        response.data
+          .map((x) => {
+            x.isChecked = false
+            if (x.EntityType === "Offering" && x.EntityID === parseInt(props.match.params.id)) {
+              x.isChecked = true
+            }
+            x.type = "Offering"
+            return x
+          })
+          .sort((x, y) => Number(y.isChecked) - Number(x.isChecked))
+      )
+    }
+  }
+
+  const searchParentTags = async () => {
+    setLoadingParentTagSearchResults(true)
+    const response = await getParentTags(convertFormFieldIntoSearchParam(formInstance.getFieldsValue()))
+    setLoadingParentTagSearchResults(false)
+    if (response.success && Array.isArray(response.data)) {
+      setParentTags(
+        response.data
+          .map((x) => {
+            x.isChecked = false
+            if (x.EntityType === "Offering" && x.EntityID === parseInt(props.match.params.id)) {
+              x.isChecked = true
+            }
+            x.Name = x.Tag
+            x.ID = x.TagID
+            x.Description = x.TagDescription
+            x.type = "Parent"
+            return x
+          })
+          .sort((x, y) => Number(y.isChecked) - Number(x.isChecked))
+      )
+    }
+  }
+
+  const addRemoveTagToOffering = async (Tag: { [key: string]: any }, add: boolean): Promise<IApiResponse> => {
+    const formValue = formInstance.getFieldsValue()
+    const methodToCall = add ? addTagIntoEntity : removeTagFromEntity
+    const loadingMethod =
+      Tag.type === "Offering" ? setLoadingOfferingTagSearchResults : setLoadingParentTagSearchResults
+    loadingMethod(true)
+    const response = await methodToCall({
+      ...formValue,
+      TagID: Tag.ID
+    })
+    loadingMethod(false)
+    return response
+  }
+
+  return (
+    <>
+      <Card>
+        <Form form={formInstance} initialValues={initialValues}>
+          <div style={hidden}>
+            <Form.Item label="Tag types" name={fieldNames.EntityType}>
+              <Input aria-label="Tag Types" />
+            </Form.Item>
+            <Form.Item label="Tag types" name={fieldNames.EntityID}>
+              <Input aria-label="Tag ID" />
+            </Form.Item>
+          </div>
+
+          <Form.Item label="Tag types" name={fieldNames.TagTypeID}>
+            <Select
+              mode="multiple"
+              aria-label="Tag Select"
+              style={{ width: "100%" }}
+              notFoundContent={loadingTagTypes ? <Spin size="small" /> : null}
+              placeholder="Please select Tag types"
+            >
+              {tagTypes.map((tag) => {
+                return (
+                  <Select.Option value={`${tag.ID};${tag.Name}`} key={tag.ID} style={{ lineHeight: "32px" }}>
+                    {tag.Name}
+                  </Select.Option>
+                )
+              })}
+            </Select>
+          </Form.Item>
+
+          <Row gutter={[48, 8]}>
+            <Col span="auto">
+              <Form.Item label="Search by Tag name" name={fieldNames.Tag}>
+                <Input aria-label="Search by Tag Name" />
+              </Form.Item>
+            </Col>
+            <Col span="auto">
+              <Form.Item label="Selected" name={fieldNames.IsSelected} valuePropName="checked">
+                <Switch
+                  aria-label="Is Selected"
+                  checkedChildren={<CheckOutlined />}
+                  unCheckedChildren={<CloseOutlined />}
+                />
+              </Form.Item>
+            </Col>
+            <Col span="auto" style={{ textAlign: "right" }}>
+              <Button
+                type="primary"
+                onClick={searchTags}
+                disabled={loadingOfferingTagSearchResults || loadingParentTagSearchResults}
+              >
+                Search
+              </Button>
+              <Button style={{ margin: "0 8px" }} type="primary" danger onClick={resetForm}>
+                Clear
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Card>
+
+      <Row>
+        <Col span={24}>
+          <TagsTable
+            title="Offering Tags"
+            data={offeringTags}
+            loading={loadingOfferingTagSearchResults}
+            select={addRemoveTagToOffering}
+          />
+        </Col>
+
+        <Col span={24}>
+          <TagsTable
+            title="Parent Tags"
+            data={parentTags}
+            loading={loadingParentTagSearchResults}
+            select={addRemoveTagToOffering}
+          />
+        </Col>
+      </Row>
+    </>
+  )
+}
