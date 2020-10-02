7ddcb4ce69b9c4a93b4aa2ca5ab03ec66c0a79b0
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2020-10-02 06:06:26 +0600

    sortable table implemented

--- app/src/Component/Question/Create/QuestionSelectionOptionForm.tsx
+++ app/src/Component/Question/Create/QuestionSelectionOptionForm.tsx
@@ -1,5 +1,40 @@
-import React from "react"
+import { Button } from "antd"
+import React, { useState } from "react"
+import FixedQuestionSortableTable from "~/Component/Question/Create/FixedQuestionSortableTable"
 
 export default function QuestionSelectionOptionForm() {
-  return <div style={{ border: "1px solid black", width: "100%", height: "300px" }}>setShowSelectionOption</div>
+  interface IOptionData {
+    SortPosition: number
+    TextValue: string
+    IsDefault: boolean
+    index: number
+  }
+
+  const [optionData, setOptionData] = useState<Array<IOptionData>>([])
+
+  const addRow = () => {
+    setOptionData([
+      ...optionData,
+      {
+        SortPosition: optionData.length + 1,
+        TextValue: "",
+        IsDefault: false,
+        index: optionData.length + 1
+      }
+    ])
+  }
+
+  return (
+    <div style={{ border: "1px solid black" }}>
+      <FixedQuestionSortableTable
+        data={optionData}
+        updatedList={(updatedData) => {
+          console.log("updatedData ", updatedData)
+        }}
+      />
+      <Button type="primary" onClick={addRow}>
+        Add
+      </Button>
+    </div>
+  )
 }
