511e288a094fc9e7c1e95ead7bcf97ac41edb42e
sabbirahmed-dsi <sabbir.ahmed@dsinnovators.com>
2020-11-26 19:43:50 +0600

    Certificate exist list.

--- app/src/Component/Registration/IssueCertificateForm.tsx
+++ app/src/Component/Registration/IssueCertificateForm.tsx
@@ -1,13 +1,14 @@
 import "~/Sass/utils.scss"
-import React, { useState } from "react"
-import { Button, Form, Spin } from "antd"
+import React, { useState, useEffect } from "react"
+import { Button, Form, Spin, Table } from "antd"
 import { ISSUE_CERTIFICATE_SAVE_SUCCESS } from "~/utils/Constants"
-import { issueCertificate, previewCertificate } from "~/ApiServices/Service/RegistrationService"
+import { issueCertificate, previewCertificate, searchCertificate } from "~/ApiServices/Service/RegistrationService"
 import Notification from "~/utils/notification"
 import { ICertificateFieldNames } from "~/Component/Registration/Interfaces"
 import { ISimplifiedApiErrorMessage } from "@packages/api/lib/utils/HandleResponse/ProcessedApiError"
 import CertificateForm from "../Certificate/CertificateForm"
 import { RESPONSE_TYPE } from "@packages/api/lib/utils/Interfaces"
+import { renderDate } from "~/Component/Common/ResponsiveTable"
 
 interface ICertificateFormProps {
   initialFormValue: { [key: string]: any }
@@ -17,9 +18,24 @@ interface ICertificateFormProps {
 export default function IssueCertificateForm(props: ICertificateFormProps) {
   const [form] = Form.useForm()
   const [loading, setLoading] = useState<boolean>(false)
+  const [certificateItems, setCertificateItems] = useState<Array<any>>([])
   const [errorMessages, setErrorMessages] = useState<Array<ISimplifiedApiErrorMessage>>([])
 
   props.initialFormValue["IsProgram"] = false
+
+  useEffect(() => {
+    ; (async function () {
+      setLoading(true)
+      const result = await searchCertificate({
+        SectionID: props.initialFormValue.SectionID,
+        StudentID: props.initialFormValue.StudentID
+      })
+      if (result.success && result.data) {
+        setCertificateItems(result.data)
+      }
+      setLoading(false)
+    })()
+  }, [props.initialFormValue])
 
   const viewCertificate = async () => {
     await form.validateFields()
@@ -61,6 +77,32 @@ export default function IssueCertificateForm(props: ICertificateFormProps) {
     setLoading(false)
   }
 
+  const columns = [
+    {
+      title: "Certificat Number",
+      dataIndex: "CertificateNumber"
+    },
+    {
+      title: "Certificat Name",
+      dataIndex: "CertificateName"
+    },
+    {
+      title: "Valid",
+      dataIndex: "PublishOnWeb",
+      render: (text: any) => (text ? "Yes" : "No")
+    },
+    {
+      title: "Issue Date",
+      dataIndex: "IssueDate",
+      render: renderDate
+    },
+    {
+      title: "Expiration Date",
+      dataIndex: "ExpirationDate",
+      render: renderDate
+    }
+  ]
+
   return (
     <Spin size="large" spinning={loading}>
       <CertificateForm
@@ -70,7 +112,19 @@ export default function IssueCertificateForm(props: ICertificateFormProps) {
         setApiCallInProgress={setLoading}
         errorMessages={errorMessages}
       />
-      <div style={{ textAlign: "center" }}>
+
+      {Object.keys(certificateItems).length > 0 && (
+        <Table
+          className="issue-certificate-data-table"
+          rowKey="StudentCertificateID"
+          bordered
+          dataSource={certificateItems}
+          pagination={false}
+          columns={columns}
+        />
+      )}
+
+      <div style={{ marginTop: "16px", textAlign: "center" }}>
         <Button type="primary" onClick={onFormSubmission}>Save Certificate</Button>
         <Button type="primary" onClick={viewCertificate} style={{ marginLeft: "16px" }}>Preview Certificate</Button>
       </div>
