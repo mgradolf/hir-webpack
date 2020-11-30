0c1ede1c6de36eb545198b4a289b579af77449eb
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2020-11-30 19:09:58 +0600

    datepicker component support url query params

--- app/src/Component/Common/SearchFilters/SearchDatePicker.tsx
+++ app/src/Component/Common/SearchFilters/SearchDatePicker.tsx
@@ -4,20 +4,30 @@ import {
   IFilterGenericComponentProps,
   SearchFieldWrapper
 } from "~/Component/Common/SearchFilters/common"
-import { DatePicker } from "antd"
+import { DatePicker, Form, Input } from "antd"
 import { DATE_FORMAT } from "~/utils/Constants"
+import moment from "moment"
 
 export function DatePickerInputType(props: IFilterGenericComponentProps<IFilterFieldObject>) {
   return (
-    <SearchFieldWrapper {...props}>
-      <DatePicker
-        allowClear
-        disabled={props.disabled}
-        onChange={(dateString) => {
-          dateString && props.formInstance.setFieldsValue({ [props.fieldName]: dateString })
-        }}
-        format={DATE_FORMAT}
-      />
-    </SearchFieldWrapper>
+    <>
+      <Form.Item className="hidden" name={props.fieldName}>
+        <Input />
+      </Form.Item>
+      <SearchFieldWrapper {...props} fieldName="">
+        <DatePicker
+          allowClear
+          disabled={props.disabled}
+          {...(props.defaultValue && {
+            defaultValue: moment(props.defaultValue, DATE_FORMAT)
+          })}
+          onChange={(date, dateString) => {
+            console.log("date", date, "dateString", dateString)
+            dateString && props.formInstance.setFieldsValue({ [props.fieldName]: dateString })
+          }}
+          format={DATE_FORMAT}
+        />
+      </SearchFieldWrapper>
+    </>
   )
 }
