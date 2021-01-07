0c28e87d955104b102b8d2c23fefdb1915206d08
siam <siam.nurnabi@dsinnovators.com>
2021-01-07 13:54:23 +0600

    financial.AmountOwedReport

--- app/src/Pages/Discovery/Report/ReportFormMeta/financial.AmountOwedReport.tsx
+++ app/src/Pages/Discovery/Report/ReportFormMeta/financial.AmountOwedReport.tsx
@@ -1,5 +1,45 @@
-import { IFilterField } from "~/Component/Common/SearchFilters/common"
+import { getOrganizations } from "~/ApiServices/Service/RefLookupService"
+import { DATE_PICKERS, DROPDOWN, IFilterField } from "~/Component/Common/SearchFilters/common"
+import { SearchOfferingLookupButton } from "~/Component/Common/SearchFilters/SearchLookups/SearchOfferingLookup"
+import { SearchSectionLookupButton } from "~/Component/Common/SearchFilters/SearchLookups/SearchSectionLookup"
+import { SearchStudentLookupButton } from "~/Component/Common/SearchFilters/SearchLookups/SearchStudentLookup"
 
-const meta: IFilterField[] = []
+const meta: IFilterField[] = [
+  {
+    label: "Offering",
+    fieldName: "OfferingID",
+    customFilterComponent: SearchOfferingLookupButton
+  },
+  {
+    label: "Section",
+    fieldName: "SectionID",
+    customFilterComponent: SearchSectionLookupButton
+  },
+  {
+    label: "Student",
+    fieldName: "StudentID",
+    customFilterComponent: SearchStudentLookupButton
+  },
+  {
+    label: "Registration Date",
+    fieldName: "FromRegistrationDate",
+    fieldName2: "ToRegistrationDate",
+    inputType: DATE_PICKERS
+  },
+  {
+    label: "Department",
+    inputType: DROPDOWN,
+    fieldName: "OrganizationID",
+    refLookupService: getOrganizations,
+    displayKey: "Name",
+    valueKey: "OrganizationID"
+  }
+]
 
 export default meta
+
+// SectionID/OfferingID = section
+// 				StudentID = student
+// 				FromRegistrationDate = Registration from
+// 				ToRegistrationDate = Registration To
+// 				OrganizationID	= Department
