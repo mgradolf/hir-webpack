02e1a1c140e1fb9d572eddc47cca8bf64c9822f7
siam <siam.nurnabi@dsinnovators.com>
2021-01-07 20:27:12 +0600

    financial.CustomerCashAccountActivity

--- app/src/Pages/Discovery/Report/ReportFormMeta/financial.CustomerCashAccountActivity.tsx
+++ app/src/Pages/Discovery/Report/ReportFormMeta/financial.CustomerCashAccountActivity.tsx
@@ -1,5 +1,23 @@
-import { IFilterField } from "~/Component/Common/SearchFilters/common"
+import { DATE_PICKERS, IFilterField } from "~/Component/Common/SearchFilters/common"
+import { SearchPersonLookupButton } from "~/Component/Common/SearchFilters/SearchLookups/SearchPersonLookup"
 
-const meta: IFilterField[] = []
+const meta: IFilterField[] = [
+  {
+    label: "Account Owner",
+    fieldName: "PersonID",
+    customFilterComponent: SearchPersonLookupButton
+  },
+  {
+    label: "Select Date",
+    inputType: DATE_PICKERS,
+
+    displayKey: "From",
+    fieldName: "TxDateFrom",
+    valueKey: "FromDate",
+    displayKey2: "To",
+    fieldName2: "TxDateTo",
+    valueKey2: "ToDate"
+  }
+]
 
 export default meta
