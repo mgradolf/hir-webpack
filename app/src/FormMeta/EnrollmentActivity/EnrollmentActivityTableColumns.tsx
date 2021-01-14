4baed0a4f031475bce2674cf01fb58ab731dfc6c
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-01-14 21:32:46 +0600

    feedback

--- app/src/FormMeta/EnrollmentActivity/EnrollmentActivityTableColumns.tsx
+++ app/src/FormMeta/EnrollmentActivity/EnrollmentActivityTableColumns.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import { getStudentEnrollmentActivity } from "~/ApiServices/Service/ActivityService"
-import { renderDate, TableColumnType } from "~/Component/Common/ResponsiveTable"
+import { renderDateTime, TableColumnType } from "~/Component/Common/ResponsiveTable"
 import { ITableConfigProp } from "~/FormMeta/ITableConfigProp"
 import { ReadOutlined } from "@ant-design/icons"
 
@@ -18,27 +18,28 @@ export const getEnrollmentActivityLogTableColumns = (isModal = false): ITableCon
         )
       })
     },
-    {
-      title: "Section Number",
-      dataIndex: "SectionNumber",
-      render: (text: any, record: any) => (isModal ? { text } : <Link to={`/section/${record.SectionID}`}>{text}</Link>)
-    },
-    {
-      title: "Offering Name",
-      dataIndex: "OfferingName",
-      render: (text: any, record: any) =>
-        isModal ? { text } : <Link to={`/offering/${record.OfferingID}`}>{text}</Link>
-    },
+    { title: "Activity Time", dataIndex: "ActivityModifiedDate", render: renderDateTime },
+    { title: "Activity Type", dataIndex: "ActivityOperation", render: undefined },
+    { title: "Activity By", dataIndex: "ActivityModifiedByName", render: undefined },
     {
       title: "Student Name",
       dataIndex: "SortName",
       render: (text: any, record: any) =>
         isModal ? { text } : <Link to={`/person/student/${record.StudentID}`}>{text}</Link>
     },
-    { title: "Enrollment Status", dataIndex: "SectionRosterStatusCodeName", render: undefined },
-    { title: "Activity Date", dataIndex: "ActivityModifiedDate", render: renderDate },
-    { title: "Activity Type", dataIndex: "ActivityOperation", render: undefined },
-    { title: "Activity By", dataIndex: "ActivityModifiedByName", render: undefined }
+    {
+      title: "Section Number",
+      dataIndex: "SectionNumber",
+      render: (text: any, record: any) => (isModal ? { text } : <Link to={`/section/${record.SectionID}`}>{text}</Link>)
+    },
+    // {
+    //   title: "Offering Name",
+    //   dataIndex: "OfferingName",
+    //   render: (text: any, record: any) =>
+    //     isModal ? { text } : <Link to={`/offering/${record.OfferingID}`}>{text}</Link>
+    // },
+
+    { title: "Enrollment Status", dataIndex: "SectionRosterStatusCodeName", render: undefined }
   ]
 
   const responsiveColumnIndices: number[] = []
