276675d1856c192b828bd9b58cbea59cd10bac4b
sabbirahmed-dsi <sabbir.ahmed@dsinnovators.com>
2021-01-28 20:26:53 +0600

    Registration search meta update.

--- app/src/FormMeta/Registration/RegistrationSearchMeta.ts
+++ app/src/FormMeta/Registration/RegistrationSearchMeta.ts
@@ -22,7 +22,7 @@ export const RegistrationSearchMeta: IFilterField[] = [
     customFilterComponent: SearchPersonLookupButton
   },
   {
-    label: "Start Date",
+    label: "Order Date",
     inputType: DATE_PICKERS,
     displayKey: "From",
 
@@ -50,17 +50,17 @@ export const RegistrationSearchMeta: IFilterField[] = [
     fieldName: "PackageID",
     customFilterComponent: SearchPackageLookupButton
   },
-  {
-    label: "Create Date",
-    inputType: DATE_PICKERS,
-    displayKey: "From",
+  // {
+  //   label: "Create Date",
+  //   inputType: DATE_PICKERS,
+  //   displayKey: "From",
 
-    fieldName: "CreatedFromDate",
-    valueKey: "CreatedFromDate",
-    ariaLabel: "Start Date From",
-    displayKey2: "To",
-    valueKey2: "CreatedToDate",
-    fieldName2: "CreatedToDate",
-    ariaLabel2: "Start Date To"
-  }
+  //   fieldName: "CreatedFromDate",
+  //   valueKey: "CreatedFromDate",
+  //   ariaLabel: "Start Date From",
+  //   displayKey2: "To",
+  //   valueKey2: "CreatedToDate",
+  //   fieldName2: "CreatedToDate",
+  //   ariaLabel2: "Start Date To"
+  // }
 ]
