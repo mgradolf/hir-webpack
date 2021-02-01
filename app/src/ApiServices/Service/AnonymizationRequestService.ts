7d5fc1c04018c7ecd4a53e56566c987baf6b9b9f
siam <siam.nurnabi@dsinnovators.com>
2021-02-01 15:41:52 +0600

    AnonymizationRequestService

--- app/src/ApiServices/Service/AnonymizationRequestService.ts
+++ app/src/ApiServices/Service/AnonymizationRequestService.ts
@@ -1,6 +1,9 @@
 import AnonymizationRequestService, { config } from "@packages/api/lib/proxy/Service/AnonymizationRequestService"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function getAnonymizeRequests(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return AnonymizationRequestService[config.Actions.getAnonymizeRequests](Params)
+export function getAnonymizeRequests(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return AnonymizationRequestService[config.Actions.getAnonymizeRequests](Params, Headers)
 }
