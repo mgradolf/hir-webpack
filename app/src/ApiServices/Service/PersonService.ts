a6cb16add8bf873d2ce441cfaa4ba89d5ddb1cfe
siam <siam.nurnabi@dsinnovators.com>
2021-02-01 17:41:26 +0600

    PersonService

--- app/src/ApiServices/Service/PersonService.ts
+++ app/src/ApiServices/Service/PersonService.ts
@@ -1,46 +1,73 @@
 import PersonService, { config } from "@packages/api/lib/proxy/Service/PersonService"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function getPersonDetails(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.getPersonDetails](Params)
+export function getPersonDetails(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.getPersonDetails](Params, Headers)
 }
 
-export function setUpWebLogin(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.setUpWebLogin](Params)
+export function setUpWebLogin(Params: { [key: string]: any }, Headers?: { [key: string]: any }): Promise<IApiResponse> {
+  return PersonService[config.Actions.setUpWebLogin](Params, Headers)
 }
 
-export function getPersonLogin(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.getPersonLogin](Params)
+export function getPersonLogin(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.getPersonLogin](Params, Headers)
 }
 
-export function getRegions(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.getRegions](Params)
+export function getRegions(Params: { [key: string]: any }, Headers?: { [key: string]: any }): Promise<IApiResponse> {
+  return PersonService[config.Actions.getRegions](Params, Headers)
 }
 
-export function removePersonEducationHistory(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.removePersonEducationHistory](Params)
+export function removePersonEducationHistory(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.removePersonEducationHistory](Params, Headers)
 }
 
-export function updatePersonEducationHistory(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.updatePersonEducationHistory](Params)
+export function updatePersonEducationHistory(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.updatePersonEducationHistory](Params, Headers)
 }
 
-export function createPersonEducationHistory(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.createPersonEducationHistory](Params)
+export function createPersonEducationHistory(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.createPersonEducationHistory](Params, Headers)
 }
 
-export function findPersonEducationHist(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.findPersonEducationHist](Params)
+export function findPersonEducationHist(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.findPersonEducationHist](Params, Headers)
 }
 
-export function getPersonDisabilities(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.getPersonDisabilities](Params)
+export function getPersonDisabilities(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.getPersonDisabilities](Params, Headers)
 }
 
-export function savePersonDisabilities(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.savePersonDisabilities](Params)
+export function savePersonDisabilities(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.savePersonDisabilities](Params, Headers)
 }
 
-export function getFacultySchedule(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PersonService[config.Actions.getFacultySchedule](Params)
+export function getFacultySchedule(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PersonService[config.Actions.getFacultySchedule](Params, Headers)
 }
