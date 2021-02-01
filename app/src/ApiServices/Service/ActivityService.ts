77cf0230495895e00b2388b20e567f41c3ff543e
siam <siam.nurnabi@dsinnovators.com>
2021-02-01 14:52:54 +0600

    ActivityService

--- app/src/ApiServices/Service/ActivityService.ts
+++ app/src/ApiServices/Service/ActivityService.ts
@@ -1,32 +1,50 @@
 import ActivityService, { config } from "@packages/api/lib/proxy/Service/ActivityService"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function getStudentAcademicActivity(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return ActivityService[config.Actions.getStudentAcademicActivity](Params)
+export function getStudentAcademicActivity(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return ActivityService[config.Actions.getStudentAcademicActivity](Params, Headers)
 }
 
-export function getStudentEnrollmentActivity(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return ActivityService[config.Actions.getStudentEnrollmentActivity](Params)
+export function getStudentEnrollmentActivity(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return ActivityService[config.Actions.getStudentEnrollmentActivity](Params, Headers)
 }
 
-export function getOrderActivity(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return ActivityService[config.Actions.getOrderActivity](Params)
+export function getOrderActivity(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return ActivityService[config.Actions.getOrderActivity](Params, Headers)
 }
 
-export function getCreditMemoActivity(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return ActivityService[config.Actions.getCreditMemoActivity](Params)
+export function getCreditMemoActivity(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return ActivityService[config.Actions.getCreditMemoActivity](Params, Headers)
 }
 
-export function getPaymentActivity(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return ActivityService[config.Actions.getPaymentActivity](Params)
+export function getPaymentActivity(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return ActivityService[config.Actions.getPaymentActivity](Params, Headers)
 }
 
-export function getSystemSchedules(Params: { [key: string]: any }): Promise<IApiResponse> {
+export function getSystemSchedules(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
   if (Params["Completed"] === "CompletedOnly") {
     Params["CompletedOnly"] = 0
   } else if (Params["Completed"] === "IncompleteOnly") {
     Params["IncompleteOnly"] = 0
   }
   delete Params["Completed"]
-  return ActivityService[config.Actions.getSystemSchedules](Params)
+  return ActivityService[config.Actions.getSystemSchedules](Params, Headers)
 }
