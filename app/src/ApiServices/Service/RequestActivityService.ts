a7f04a32038becbe45d8e95c39cb125b70c74964
siam <siam.nurnabi@dsinnovators.com>
2021-02-01 18:01:11 +0600

    "RequestActivityService"

--- app/src/ApiServices/Service/RequestActivityService.ts
+++ app/src/ApiServices/Service/RequestActivityService.ts
@@ -1,18 +1,27 @@
 import RequestActivityService, { config } from "@packages/api/lib/proxy/Service/RequestActivityService"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function bulkUpdate(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return RequestActivityService[config.Actions.bulkUpdate](Params)
+export function bulkUpdate(Params: { [key: string]: any }, Headers?: { [key: string]: any }): Promise<IApiResponse> {
+  return RequestActivityService[config.Actions.bulkUpdate](Params, Headers)
 }
 
-export function makeExternalPayment(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return RequestActivityService[config.Actions.makeExternalPayment](Params)
+export function makeExternalPayment(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return RequestActivityService[config.Actions.makeExternalPayment](Params, Headers)
 }
 
-export function extendExpirationDate(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return RequestActivityService[config.Actions.extendExpirationDate](Params)
+export function extendExpirationDate(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return RequestActivityService[config.Actions.extendExpirationDate](Params, Headers)
 }
 
-export function findPublishedAndActiveQuestionsWithOptions(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return RequestActivityService[config.Actions.findPublishedAndActiveQuestionsWithOptions](Params)
+export function findPublishedAndActiveQuestionsWithOptions(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return RequestActivityService[config.Actions.findPublishedAndActiveQuestionsWithOptions](Params, Headers)
 }
