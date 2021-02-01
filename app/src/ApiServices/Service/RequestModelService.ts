6d35bc627360560eb0fd9c4213512a121fc96087
siam <siam.nurnabi@dsinnovators.com>
2021-02-01 18:02:10 +0600

    "RequestModelService"

--- app/src/ApiServices/Service/RequestModelService.ts
+++ app/src/ApiServices/Service/RequestModelService.ts
@@ -1,10 +1,10 @@
 import RequestModelService, { config } from "@packages/api/lib/proxy/Service/RequestModelService"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function retry(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return RequestModelService[config.Actions.retry](Params)
+export function retry(Params: { [key: string]: any }, Headers?: { [key: string]: any }): Promise<IApiResponse> {
+  return RequestModelService[config.Actions.retry](Params, Headers)
 }
 
-export function cancel(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return RequestModelService[config.Actions.cancel](Params)
+export function cancel(Params: { [key: string]: any }, Headers?: { [key: string]: any }): Promise<IApiResponse> {
+  return RequestModelService[config.Actions.cancel](Params, Headers)
 }
