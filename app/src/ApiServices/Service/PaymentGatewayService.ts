c7c1aceca46f8d34499c84afb76615d0aa656375
siam <siam.nurnabi@dsinnovators.com>
2021-02-01 17:39:14 +0600

    PaymentGatewayService

--- app/src/ApiServices/Service/PaymentGatewayService.ts
+++ app/src/ApiServices/Service/PaymentGatewayService.ts
@@ -1,6 +1,9 @@
 import PaymentGatewayService, { config } from "@packages/api/lib/proxy/Service/PaymentGatewayService"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function findPaymentGatewayActivities(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return PaymentGatewayService[config.Actions.findPaymentGatewayActivities](Params)
+export function findPaymentGatewayActivities(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return PaymentGatewayService[config.Actions.findPaymentGatewayActivities](Params, Headers)
 }
