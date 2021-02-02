257a430c95974cfaab8c98cbb5b20874a5e403ea
siam <siam.nurnabi@dsinnovators.com>
2021-02-02 19:44:44 +0600

    "orderIf"

--- app/src/ApiServices/BizApi/order/orderIf.ts
+++ app/src/ApiServices/BizApi/order/orderIf.ts
@@ -1,15 +1,24 @@
 import OrderIf, { config } from "@packages/api/lib/proxy/BizApi/order/orderIf"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function findOrders(Params: any[]): Promise<IApiResponse> {
-  return OrderIf[config.Actions.findOrders](Params)
+export function findOrders(Params: { [key: string]: any }, Headers?: { [key: string]: any }): Promise<IApiResponse> {
+  return OrderIf[config.Actions.findOrders]([Params], Headers)
 }
-export function findOrderDetails(Params: any[]): Promise<IApiResponse> {
-  return OrderIf[config.Actions.findOrderDetails](Params)
+export function findOrderDetails(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return OrderIf[config.Actions.findOrderDetails]([Params], Headers)
 }
-export function findOrderLineWiseBalance(Params: any[]): Promise<IApiResponse> {
-  return OrderIf[config.Actions.findOrderLineWiseBalance](Params)
+export function findOrderLineWiseBalance(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return OrderIf[config.Actions.findOrderLineWiseBalance]([Params.OrderID], Headers)
 }
-export function findCreditMemosOrderLines(Params: any[]): Promise<IApiResponse> {
-  return OrderIf[config.Actions.findCreditMemosOrderLines](Params)
+export function findCreditMemosOrderLines(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return OrderIf[config.Actions.findCreditMemosOrderLines]([Params], Headers)
 }
