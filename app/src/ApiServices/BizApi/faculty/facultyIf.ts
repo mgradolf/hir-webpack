a75516b5571b9b9bb25d3087a691e96be54c7b3e
siam <siam.nurnabi@dsinnovators.com>
2021-02-02 18:59:32 +0600

    "facultyIf"

--- app/src/ApiServices/BizApi/faculty/facultyIf.ts
+++ app/src/ApiServices/BizApi/faculty/facultyIf.ts
@@ -1,8 +1,9 @@
 import FacultyIf, { config } from "@packages/api/lib/proxy/BizApi/faculty/facultyIf"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
-/* -------------------------------------------------------------------------- */
-/*                              offering requisite section                              */
-/* -------------------------------------------------------------------------- */
-export function searchFaculties(Params: { [key: string]: any }): Promise<IApiResponse> {
-  return FacultyIf[config.Actions.searchFaculties]([Params])
+
+export function searchFaculties(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return FacultyIf[config.Actions.searchFaculties]([Params], Headers)
 }
