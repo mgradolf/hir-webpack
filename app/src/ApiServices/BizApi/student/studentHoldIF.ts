cc304af99b90d2c5b06aa760bdca93ce77d43c3f
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-02-03 16:26:03 +0600

    studentIF

--- app/src/ApiServices/BizApi/student/studentHoldIF.ts
+++ app/src/ApiServices/BizApi/student/studentHoldIF.ts
@@ -1,6 +1,12 @@
 import StudentHoldIF, { config } from "@packages/api/lib/proxy/BizApi/student/studentHoldIF"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 
-export function findStudentHold(Params: any, ...args: any): Promise<IApiResponse> {
-  return StudentHoldIF[config.Actions.findStudentHold]([Params, null, null, null, null, null, null, null, null, null])
+export function findStudentHold(
+  Params: { [key: string]: any },
+  Headers?: { [key: string]: any }
+): Promise<IApiResponse> {
+  return StudentHoldIF[config.Actions.findStudentHold](
+    [Params, null, null, null, null, null, null, null, null, null],
+    Headers
+  )
 }
