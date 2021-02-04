2e1e64aaf870bd2d1ca444fde744b173c7d4f2e2
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-02-04 16:25:02 +0600

    multi checkbox

--- app/src/Component/Common/Form/index.tsx
+++ app/src/Component/Common/Form/index.tsx
@@ -10,7 +10,8 @@ import {
   TEXT,
   BOOLEAN,
   IField,
-  CUSTOM_FIELD
+  CUSTOM_FIELD,
+  MULTI_SELECT_CHECKBOX
 } from "~/Component/Common/Form/common"
 import { FormInput } from "~/Component/Common/Form/FormInput"
 import { FormDropDown } from "~/Component/Common/Form/FormDropDown"
@@ -21,6 +22,7 @@ import { FormCheckbox } from "~/Component/Common/Form/FormCheckbox"
 import { querystringToObject } from "~/utils/QueryStringToObjectConverter"
 import { objectToQueryString } from "~/utils/ObjectToQueryStringConverter"
 import { FormInstance } from "antd/lib/form"
+import { FormMultipleCheckbox } from "./FormMultipleCheckbox"
 
 interface IFilterColumnProps {
   meta: IField[]
@@ -167,7 +169,16 @@ export function CustomForm({
 
   return (
     <Col className={`gutter-row ${styles.offeringFilter}`} xs={24} sm={24} md={24}>
-      <Form layout="horizontal" initialValues={props.initialFormValue} form={formInstance} scrollToFirstError>
+      <Form
+        layout="horizontal"
+        initialValues={props.initialFormValue}
+        form={formInstance}
+        scrollToFirstError
+        style={{
+          maxHeight: "80vh",
+          overflowY: "scroll"
+        }}
+      >
         <SearchFormFields
           meta={props.meta}
           formInstance={formInstance}
@@ -231,6 +242,12 @@ const SearchFormFields = (props: {
               return (
                 <Col key={1000 + i} lg={12} md={12} sm={12} xs={24}>
                   <FormCheckbox {...field} key={i} formInstance={props.formInstance} />
+                </Col>
+              )
+            case MULTI_SELECT_CHECKBOX:
+              return (
+                <Col key={1000 + i} lg={12} md={12} sm={12} xs={24}>
+                  <FormMultipleCheckbox {...field} key={i} formInstance={props.formInstance} />
                 </Col>
               )
             case DROPDOWN:
--- app/src/FormMeta/User/UserFormMeta.tsx
+++ app/src/FormMeta/User/UserFormMeta.tsx
@@ -1,19 +1,19 @@
-import { IField, MULTI_SELECT_DROPDOWN, TEXT } from "~/Component/Common/Form/common"
+import { IField, MULTI_SELECT_CHECKBOX, TEXT } from "~/Component/Common/Form/common"
 import { findAllUserRoles } from "~/ApiServices/Service/UserService"
 
 export const UserSearchMeta: IField[] = [
   { label: "UserID", inputType: TEXT, fieldName: "UserID" },
   { label: "FirstName", inputType: TEXT, fieldName: "FirstName" },
   { label: "LastName", inputType: TEXT, fieldName: "LastName" },
-  { label: "MiddleName", inputType: TEXT, fieldName: "MiddleName" },
-  { label: "Email", inputType: TEXT, fieldName: "Email" },
-  { label: "Password", inputType: TEXT, fieldName: "Password" },
   {
     label: "Roles",
-    inputType: MULTI_SELECT_DROPDOWN,
+    inputType: MULTI_SELECT_CHECKBOX,
     fieldName: "Roles",
     refLookupService: () => findAllUserRoles({}),
     displayKey: "RoleName",
     valueKey: "RoleName"
-  }
+  },
+  { label: "MiddleName", inputType: TEXT, fieldName: "MiddleName" },
+  { label: "Email", inputType: TEXT, fieldName: "Email" },
+  { label: "Password", inputType: TEXT, fieldName: "Password" }
 ]
