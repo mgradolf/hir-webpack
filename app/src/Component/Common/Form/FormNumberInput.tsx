5540fa30e21f933f7a322fbfd21d7a720c1feacb
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-02-11 20:54:49 +0600

    waitlist

--- app/src/Component/Common/Form/FormNumberInput.tsx
+++ app/src/Component/Common/Form/FormNumberInput.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { SearchFieldWrapper, IGeneratedField } from "~/Component/Common/Form/common"
 import { Input } from "antd"
 
-export function FormInput(props: IGeneratedField) {
+export function FormNumberInput(props: IGeneratedField) {
   return (
     <SearchFieldWrapper {...props}>
       <Input aria-label={props.ariaLabel} type="text" disabled={props.disabled} />
