b4f180c3b73a8b9d0412b9ebd03dcfb39ee1be9e
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-02-18 17:45:15 +0600

    random

--- app/src/HelpFileMap.ts
+++ app/src/HelpFileMap.ts
@@ -1,3 +1,7 @@
+export const host_url = "https://docs.google.com/document/d/"
 export const helpFileNameMap = {
-  generic: "1FKV-i5gsVClhsHLYFMqpdEGDVZmwJU576AXKKcTfwiY/edit?usp=sharing"
+  generic: "1FKV-i5gsVClhsHLYFMqpdEGDVZmwJU576AXKKcTfwiY/edit?usp=sharing",
+  offering: "1FKV-i5gsVClhsHLYFMqpdEGDVZmwJU576AXKKcTfwiY/edit?usp=sharing",
+  section: "1FKV-i5gsVClhsHLYFMqpdEGDVZmwJU576AXKKcTfwiY/edit?usp=sharing",
+  person: "1FKV-i5gsVClhsHLYFMqpdEGDVZmwJU576AXKKcTfwiY/edit?usp=sharing"
 }
