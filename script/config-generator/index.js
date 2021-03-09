c287da1ae97db8449c36639c8697b5e934b270d2
DSi <dsi@DSis-iMac.local>
2021-03-09 19:19:53 +0600

    web-inf folder created

---
+++ script/config-generator/index.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+const getAllFiles = function (dirPath, arrayOfFiles) {
+	files = fs
+		.readdirSync(dirPath)
+		.filter((x) => !(x.includes(".ts") || x.includes(".map")));
+
+	arrayOfFiles = arrayOfFiles || [];
+
+	files.forEach(function (file) {
+		console.log("dirpath ", dirPath);
+		if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+			arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+		} else {
+			arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+		}
+	});
+
+	return arrayOfFiles;
+};
+
+const generateSearchMetaConfig = (fileNames, jsonFileName) => {
+	// const dirPath = "../../packages/api/lib/proxy/Service";
+	// const fileNames = fs
+	// 	.readdirSync(dirPath)
+	// 	.filter((x) => !(x.includes(".ts") || x.includes(".map")));
+
+	let apiConfigList = [];
+	for (fileName of fileNames) {
+		console.log(fileName);
+		const { config } = require(fileName);
+		console.log(config);
+		apiConfigList.push(config);
+	}
+
+	fs.writeFileSync(
+		`./nextRelease/${jsonFileName}.json`,
+		JSON.stringify(apiConfigList)
+	);
+};
+
+const searchMetas = getAllFiles(
+	"../../packages/api/lib/proxy/Service"
+);
+
+
+generateSearchMetaConfig(searchMetas, "bizapi");
