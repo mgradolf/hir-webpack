e568706c55059ec6d9657505c1a26de30d5cbde0
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-03-31 21:34:50 +0600

    dropdown is searchable

--- app/src/Component/Common/Form/FormDropDown.tsx
+++ app/src/Component/Common/Form/FormDropDown.tsx
@@ -51,18 +51,22 @@ export function FormDropDown(props: IGeneratedField & { onChangeCallback?: (para
   return (
     <SearchFieldWrapper {...props}>
       <Select
+        showSearch
         allowClear={true}
         loading={loading}
+        filterOption={(inputValue, options) => {
+          return !!(
+            options &&
+            typeof options.children === "string" &&
+            options.children.toLowerCase().includes(inputValue.toLowerCase())
+          )
+        }}
         aria-label={props.ariaLabel}
         disabled={props.disabled}
         onChange={props.onChangeCallback}
       >
         {options &&
-          options.map(({ label, value }, i) => (
-            <Select.Option value={value} key={`${value}_${i}`}>
-              {label}
-            </Select.Option>
-          ))}
+          options.map(({ label, value }, i) => <Select.Option value={value} key={`${value}_${i}`} children={label} />)}
       </Select>
     </SearchFieldWrapper>
   )
