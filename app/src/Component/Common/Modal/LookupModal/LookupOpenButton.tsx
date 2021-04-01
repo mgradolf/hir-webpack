c6425878ce94bcc3adc8705d5b7779be266be960
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-04-01 19:04:30 +0600

    search lookup on typing

--- app/src/Component/Common/Modal/LookupModal/LookupOpenButton.tsx
+++ app/src/Component/Common/Modal/LookupModal/LookupOpenButton.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
-import { Col, Row, Select } from "antd"
+import { Col, Row, Select, Spin } from "antd"
 import { IField, IGeneratedField, SearchFieldWrapper } from "~/Component/Common/Form/common"
 import { LookupModal } from "~/Component/Common/Modal/LookupModal/LookupModal"
 import { IApiResponse } from "@packages/api/lib/utils/Interfaces"
 import { TableColumnType } from "~/Component/Common/ResponsiveTable"
 import { SearchOutlined } from "@ant-design/icons"
+import debounce from "~/utils/debounce"
 
 export interface ILookupOpenButton extends IGeneratedField {
   displayKey: string
@@ -25,10 +26,21 @@ export interface ILookupOpenButton extends IGeneratedField {
 export function LookupOpenButton(props: ILookupOpenButton) {
   const [showModal, setShowModal] = useState(false)
   const [options, setOptions] = useState<any[]>([])
+  const [loading, setLoading] = useState(false)
 
   const closeModal = (items?: any[]) => {
     if (items && items.length > 0) {
-      setOptions(items)
+      if (props.extraProps && props.extraProps.isArray) {
+        const previousValues: any[] = props.formInstance.getFieldValue(props.fieldName) || []
+        console.log("previouse values ", previousValues)
+        props.formInstance.setFieldsValue({
+          [props.fieldName]: [...previousValues, ...items.map((x) => x[props.valueKey])]
+        })
+        setOptions([...options, ...items])
+      } else {
+        setOptions(items)
+        props.formInstance.setFieldsValue({ [props.fieldName]: items[0][props.valueKey] })
+      }
     }
     if (props.onSelectedItems) {
       props.onSelectedItems(items)
@@ -36,6 +48,17 @@ export function LookupOpenButton(props: ILookupOpenButton) {
     setShowModal(false)
   }
 
+  const handleSearch = debounce((searchInput: any): void => {
+    if (!searchInput || searchInput === "") return
+    setLoading(true)
+    props.searchFunc({ [props.displayKey]: searchInput }).then((x) => {
+      if (x.success) {
+        console.log(x.data)
+        setOptions(x.data)
+      }
+      setLoading(false)
+    })
+  }, 500)
   const toRender = (
     <>
       <Row>
@@ -50,18 +73,23 @@ export function LookupOpenButton(props: ILookupOpenButton) {
           >
             <Select
               showSearch
+              loading={loading}
               allowClear={true}
+              filterOption={false}
               aria-label={props.ariaLabel}
               disabled={props.disabled}
+              notFoundContent={loading ? <Spin size="small" /> : null}
               {...(props.extraProps && props.extraProps.isArray && { mode: "multiple" })}
-              listItemHeight={0}
-              listHeight={0}
+              listItemHeight={10}
+              listHeight={256}
+              onSearch={handleSearch}
             >
-              {options.map((x, i) => (
-                <Select.Option value={x[props.valueKey]} key={`${x[props.valueKey]}_${i}`}>
-                  {x[props.displayKey]}
-                </Select.Option>
-              ))}
+              {Array.isArray(options) &&
+                options.map((x, i) => (
+                  <Select.Option value={x[props.valueKey]} key={`${x[props.valueKey]}_${i}`}>
+                    {x[props.displayKey]}
+                  </Select.Option>
+                ))}
             </Select>
           </SearchFieldWrapper>
         </Col>
