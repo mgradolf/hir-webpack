21d871911244ca235fb8033c8aa7497975870f5e
Muhammed Tareq Aziz <tareq.aziz@deligram.com>
2021-05-20 16:04:49 +0600

    create payment button in payment page

--- app/src/Pages/Manage/Financials/PaymentPage.tsx
+++ app/src/Pages/Manage/Financials/PaymentPage.tsx
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import { SearchPage } from "~/Component/Common/Page/SearchPage"
 import { getPaymentTableColumns } from "~/TableSearchMeta/Payment/PaymentTableColumns"
 import { PaymentSearchMeta } from "~/TableSearchMeta/Payment/PaymentSearchMeta"
+import { IconButton } from "~/Component/Common/Form/Buttons/IconButton"
+import { Redirect } from "react-router"
 
 export default function Payments() {
+  const [redirectToPaymentPage, setRedirectToPaymentPage] = useState<string>()
   return (
-    <SearchPage
-      title="Manage Payments"
-      meta={PaymentSearchMeta}
-      metaName="PaymentSearchMeta"
-      hideSearchField={false}
-      tableProps={getPaymentTableColumns(false)}
-    ></SearchPage>
+    <>
+      {redirectToPaymentPage && <Redirect to={redirectToPaymentPage} />}
+      <SearchPage
+        title="Manage Payments"
+        blocks={[
+          <IconButton
+            iconType="create"
+            toolTip="Make Payment"
+            onClick={() => {
+              setRedirectToPaymentPage("/create-payment")
+            }}
+          />
+        ]}
+        meta={PaymentSearchMeta}
+        metaName="PaymentSearchMeta"
+        hideSearchField={false}
+        tableProps={getPaymentTableColumns(false)}
+      />
+    </>
   )
 }
